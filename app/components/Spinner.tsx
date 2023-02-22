9ea5c522ab13937197340df6a0f876c1f0c416e0
hosmi <koike.engineer@gmail.com>
2023-02-23 07:07:05 +0900

    remove:🔥 remove console.log()

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -2,8 +2,6 @@ import { FC } from 'react'
 import styles from './Spinner.module.css'
 
 const Spinner: FC = () => {
-  console.log('spiner')
-
   return (
     <div className={styles.container}>
       <div className={styles.spiner} />
